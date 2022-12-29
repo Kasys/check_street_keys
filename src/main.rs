231fb07d8ea91d017a19dfd5af578f44cf279599
Joscha Hartmann <joschahartmann@gmx.de>
2022-12-29 15:43:45 +0100

    improve error messages improve performance add *.txt to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-#![allow(dead_code)]
+#![feature(iter_intersperse)]
 
 use std::{
-    collections::HashMap,
+    collections::{hash_map::Entry, HashMap},
     fs::File,
     io::{self, BufRead},
     str::FromStr,
 };
-
-const ERSTE: &str = "11111111111111111|Am Tiergarten";
-const ZWEITE: &str = "21111111111111111|Am Tiergarten";
+use thiserror::Error;
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Straße {
@@ -17,37 +15,58 @@ struct Straße {
 }
 
 impl std::str::FromStr for Straße {
-    type Err = String;
+    type Err = StraßeParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let teile: Vec<_> = s.split('|').collect();
-        //check for 2 parts
-        if teile.len() != 2 {
-            return Err(format!(
-                "Eintrag besteht aus {} Teilen, sollte aus 2 bestehen",
-                teile.len()
-            ));
-        }
-        let id = teile[0];
-        let name = teile[1];
-        //check id length
-        if id.chars().count() != 17 {
-            return Err(format!(
-                "id hat Länge {}, sollte 17 lang sein, straßenname: {}",
-                id.chars().count(),
-                name
-            ));
-        }
+        //match on parts len
+        match teile.len() {
+            0 => Err(StraßeParseError::Leer),
+            1 => Err(StraßeParseError::KeinName {
+                id: teile[0].to_string(),
+            }),
+            2 => {
+                let id = teile[0].to_string();
+                let name = teile[1].to_string();
+                //check id length
+                if id.chars().count() != 17 {
+                    return Err(StraßeParseError::FalscheIdLänge { id, name });
+                }
 
-        //create Straße
-        Ok(Straße {
-            id: id.to_string(),
-            name: name.to_string(),
-        })
+                //create Straße
+                Ok(Straße { id, name })
+            }
+            _ => {
+                let mut teile = teile.into_iter();
+                Err(StraßeParseError::ZuVieleTeile {
+                    id: teile.next().unwrap().to_string(),
+                    name: teile.next().unwrap().to_string(),
+                    rest: teile.intersperse("|").collect(),
+                })
+            }
+        }
     }
 }
 
+#[derive(Error, Debug)]
+enum StraßeParseError {
+    #[error("str ist leer")]
+    Leer,
+    #[error("Straßenname fehlt; id: {id}")]
+    KeinName { id: String },
+    #[error("Id hat falsche Länge (ist {}, erwarte 17); id: {id}, name: {name}", id.len())]
+    FalscheIdLänge { id: String, name: String },
+    #[error("Straße hat zu viele Teile, erwarte 2; id: {id}, name: {name}, rest: {rest}")]
+    ZuVieleTeile {
+        id: String,
+        name: String,
+        rest: String,
+    },
+}
+
 fn map_aus_datei(dateiname: &str) -> HashMap<Straße, bool> {
+    print!("Erstelle map aus {dateiname}...");
+
     let file = File::open(dateiname).unwrap();
     let lines = io::BufReader::new(file).lines();
 
@@ -59,10 +78,12 @@ fn map_aus_datei(dateiname: &str) -> HashMap<Straße, bool> {
             Err(fehler) => panic!("Straße hat falsches Format:\n{fehler}"),
         };
     }
+    println!("   Fertig!");
     map
 }
 
 fn datei_prüfen(map: &mut HashMap<Straße, bool>, dateiname: &str) {
+    println!("Prüfe {dateiname}");
     let file = File::open(dateiname).unwrap();
     let lines = io::BufReader::new(file).lines();
 
@@ -70,25 +91,29 @@ fn datei_prüfen(map: &mut HashMap<Straße, bool>, dateiname: &str) {
     for line in lines {
         let straße = Straße::from_str(line.unwrap().as_str());
         match straße {
-            Ok(straße) => {
-                if map.contains_key(&straße) {
-                    map.insert(straße, true);
-                } else {
+            Ok(straße) => match map.entry(straße) {
+                Entry::Occupied(mut e) => {
+                    e.insert(true);
+                }
+                Entry::Vacant(e) => {
+                    let straße = e.into_key();
                     println!(
-                        "Straße existiert nicht. id: {}, name: {}",
+                        "Straße existiert nicht; id: {}, name: {}",
                         straße.id, straße.name
                     );
                 }
-            }
-            Err(fehler) => println!("Fehler beim Einlesen einer Straße: {fehler}"), //TODO hilfreiche informationen ausgeben
+            },
+            Err(fehler) => println!("Fehler beim Einlesen einer Straße: {fehler}"),
         };
     }
-    println!("\nErgebnis");
+    println!("Fertig!");
+    println!("Ergebnis");
     for (straße, gefunden) in map {
         if !*gefunden {
-            println!("Straße fehlt. id: {}, name: {}", straße.id, straße.name)
+            println!("Straße fehlt; id: {}, name: {}", straße.id, straße.name)
         }
     }
+    println!("Fertig!");
 }
 
 fn main() {
